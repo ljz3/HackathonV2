d18f13012e1e9c4fef17071dc6de0b678ebddd7f
unknown <kevin.ottawa@hotmail.com>
2020-02-15 17:00:37 -0500

    Fixed output

--- app/src/main/java/tech/budgetforthemasses/budgetforthemasses/MainOutput.java
+++ app/src/main/java/tech/budgetforthemasses/budgetforthemasses/MainOutput.java
@@ -1,127 +1,29 @@
 package tech.budgetforthemasses.budgetforthemasses;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
-import java.util.ArrayList;
-import java.util.List;
-import android.content.Intent;
-import lecho.lib.hellocharts.model.PieChartData;
-import lecho.lib.hellocharts.model.SliceValue;
-import lecho.lib.hellocharts.view.PieChartView;
-
-
-public class MainOutput extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    PieChartView pieChartView;
-
-    DrawerLayout drawer;
-    NavigationView navigationView;
-    Toolbar toolbar = null;
+public class MainOutput extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_output);
+        setContentView(R.layout.content_output);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Intent i = getIntent();
-        int food = i.getIntExtra("food", -1);
-        int rent = i.getIntExtra("rent", -1);
-        int util = i.getIntExtra("util", -1);
-        int want = i.getIntExtra("want", -1);
-        int saved = i.getIntExtra("saved", -1);
-        int leftover = i.getIntExtra("leftover", -1);
 
-        pieChartView = findViewById(R.id.chart);
-
-        List pieData = new ArrayList<>();
-        pieData.add(new SliceValue(food, Color.BLUE).setLabel("Food"));
-        pieData.add(new SliceValue(rent, Color.GRAY).setLabel("Rent"));
-        pieData.add(new SliceValue(util, Color.RED).setLabel("Utilities"));
-        pieData.add(new SliceValue(want, Color.GREEN).setLabel("Wants"));
-        pieData.add(new SliceValue(saved, Color.MAGENTA).setLabel("Saved"));
-        pieData.add(new SliceValue(leftover, Color.WHITE).setLabel("Leftover"));
-
-        PieChartData pieChartData = new PieChartData(pieData);
-        pieChartData.setHasLabels(true).setValueLabelTextSize(14);
-        pieChartData.setHasCenterCircle(true).setCenterText1("Your Spending").setCenterText1FontSize(20).setCenterText1Color(Color.parseColor("#0097A7"));
-        pieChartView.setPieChartData(pieChartData);
-
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        //here is the main place where we need to work on.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_settings) {
-
-        } else if (id == R.id.nav_financials) {
-            Intent m = new Intent(MainOutput.this, MainOutput.class);
-            startActivity(m);
-        } else if (id == R.id.nav_overview) {
-            Intent j = new Intent(MainOutput.this, MainActivity.class);
-            startActivity(j);
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 }
-
