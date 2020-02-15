fcc945fd78b5ae20bc477458deb05fc014496308
unknown <kevin.ottawa@hotmail.com>
2020-02-15 04:54:18 -0500

    Java for Spinner

---
+++ app/src/main/java/tech/budgetforthemasses/budgetforthemasses/MainContent.java
@@ -0,0 +1,15 @@
+package tech.budgetforthemasses.budgetforthemasses;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+public class MainContent extends AppCompatActivity {
+    List<RowItem> rowItems;
+
+}
