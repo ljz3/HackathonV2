a42dd256950e3152ec55398b7da65eb63390c652
Jawad Arshad <56083056+HamJaw1432@users.noreply.github.com>
2020-02-15 10:20:26 -0500

    Turns input data in to usable ints

--- app/src/main/java/tech/budgetforthemasses/budgetforthemasses/MainContent.java
+++ app/src/main/java/tech/budgetforthemasses/budgetforthemasses/MainContent.java
@@ -1,23 +1,50 @@
 package tech.budgetforthemasses.budgetforthemasses;
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.view.LayoutInflater;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
-import java.util.ArrayList;
-import java.util.List;
 public class MainContent extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
+
+        Button submit_button = (Button)findViewById(R.id.submit_button);
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText income_edit = (EditText) findViewById(R.id.income_edit);
+                EditText rent_edit = (EditText) findViewById(R.id.rent_edit);
+                EditText gas_edit = (EditText) findViewById(R.id.gas_edit);
+                EditText water_edit = (EditText) findViewById(R.id.water_edit);
+                EditText electricity_edit = (EditText) findViewById(R.id.electricity_edit);
+                EditText phone_bill_edit = (EditText) findViewById(R.id.phone_bill_edit);
+                EditText food_cost_edit = (EditText) findViewById(R.id.food_cost_edit);
+                EditText entertainment_edit = (EditText) findViewById(R.id.entertainment_edit);
+                EditText hobbies_edit = (EditText) findViewById(R.id.hobbies_edit);
+                EditText others_edit = (EditText) findViewById(R.id.others_edit);
+                EditText savings_edit = (EditText) findViewById(R.id.savings_edit);
+
+                int income = Integer.parseInt(income_edit.getText().toString());
+                int rent = Integer.parseInt(rent_edit.getText().toString());
+                int gas = Integer.parseInt(gas_edit.getText().toString());
+                int water = Integer.parseInt(water_edit.getText().toString());
+                int electricity = Integer.parseInt(electricity_edit.getText().toString());
+                int phone_bill = Integer.parseInt(phone_bill_edit.getText().toString());
+                int food_cost = Integer.parseInt(food_cost_edit.getText().toString());
+                int entertainment = Integer.parseInt(entertainment_edit.getText().toString());
+                int hobbies = Integer.parseInt(hobbies_edit.getText().toString());
+                int others = Integer.parseInt(others_edit.getText().toString());
+                int savings = Integer.parseInt(savings_edit.getText().toString());
+
+                
+
+
+            }
+        });
 
     }
 
