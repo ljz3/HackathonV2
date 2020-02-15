d4cdaa5a13a8798d7941bfe37cafbe687d31b91c
unknown <kevin.ottawa@hotmail.com>
2020-02-15 14:48:35 -0500

    Found error on pie chart

--- app/src/main/java/tech/budgetforthemasses/budgetforthemasses/MainContent.java
+++ app/src/main/java/tech/budgetforthemasses/budgetforthemasses/MainContent.java
@@ -10,17 +10,17 @@ import android.widget.RadioButton;
 
 public class MainContent extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
 
-        Button submit_button = (Button)findViewById(R.id.submit_button);
+        Button submit_button = (Button) findViewById(R.id.submit_button);
         submit_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 EditText income_edit = (EditText) findViewById(R.id.income_edit);
-                RadioButton time_frame = (RadioButton) findViewById(R.id.radioBiWeekly) ;
+                RadioButton time_frame = (RadioButton) findViewById(R.id.radioBiWeekly);
                 EditText rent_edit = (EditText) findViewById(R.id.rent_edit);
                 EditText gas_edit = (EditText) findViewById(R.id.gas_edit);
                 EditText water_edit = (EditText) findViewById(R.id.water_edit);
@@ -33,8 +33,7 @@ public class MainContent extends AppCompatActivity {
                 EditText savings_edit = (EditText) findViewById(R.id.savings_edit);
 
                 int income = Integer.parseInt(income_edit.getText().toString());
-                if(time_frame.isChecked())
-                {
+                if (time_frame.isChecked()) {
                     income = income * 2;
                 }
                 int rent = Integer.parseInt(rent_edit.getText().toString());
@@ -53,27 +52,24 @@ public class MainContent extends AppCompatActivity {
                 int wants = entertainment + hobbies + others;
                 int leftover = income - food_cost - rent - utilities - wants - savings;
 
-                float precent_food = ((float)food_cost / (float)income)*100;
-                float precent_rent = ((float)rent / (float)income)*100;
-                float precent_util = ((float)utilities / (float)income)*100;
-                float precent_wants = ((float)wants / (float)income)*100;
-                float precent_saved = ((float)savings / (float)income)*100;
-                float precent_leftover = ((float)leftover / (float)income)*100;
+                float precent_food = ((float) food_cost / (float) income) * 100;
+                float precent_rent = ((float) rent / (float) income) * 100;
+                float precent_util = ((float) utilities / (float) income) * 100;
+                float precent_wants = ((float) wants / (float) income) * 100;
+                float precent_saved = ((float) savings / (float) income) * 100;
+                float precent_leftover = ((float) leftover / (float) income) * 100;
 
-<<<<<<< HEAD
-                Intent move_to_output = new Intent(getApplicationContext(),MainOutput.class);
+                Intent move_to_output = new Intent(getApplicationContext(), MainOutput.class);
 
                 startActivity(move_to_output);
-=======
-               Intent intent = new Intent(MainContent.this,MainOutput.class);
-               intent.putExtra("food", precent_food);
+                Intent intent = new Intent(MainContent.this, MainOutput.class);
+                intent.putExtra("food", precent_food);
                 intent.putExtra("rent", precent_rent);
                 intent.putExtra("util", precent_util);
                 intent.putExtra("want", precent_wants);
                 intent.putExtra("saved", precent_saved);
                 intent.putExtra("leftover", precent_leftover);
                 startActivity(intent);
->>>>>>> 4d965a5aaa64ca6b4493c4cf7976fdc64676025d
 
             }
         });
@@ -81,5 +77,8 @@ public class MainContent extends AppCompatActivity {
     }
 
 
-
+    public void goOutput(View view) {
+        Intent intent = new Intent(MainContent.this, MainOutput.class);
+        startActivity(intent);
+    }
 }
