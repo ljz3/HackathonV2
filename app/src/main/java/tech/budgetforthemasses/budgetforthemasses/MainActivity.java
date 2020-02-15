c8fcab276394863d519fb6b8a15d70dec7ae8fa9
KhaledElbasiouni222 <khaled.elbasiouni222@gmail.com>
2020-02-15 04:31:04 -0500

    Changed Navbar

--- app/src/main/java/tech/budgetforthemasses/budgetforthemasses/MainActivity.java
+++ app/src/main/java/tech/budgetforthemasses/budgetforthemasses/MainActivity.java
@@ -80,18 +80,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_settings) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_financials) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_overview) {
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
