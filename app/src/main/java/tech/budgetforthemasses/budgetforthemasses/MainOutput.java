7d285f5b8d1661f23abba3497867e92d2774b308
Jawad Arshad <56083056+HamJaw1432@users.noreply.github.com>
2020-02-15 10:52:34 -0500

    started intent

---
+++ app/src/main/java/tech/budgetforthemasses/budgetforthemasses/MainOutput.java
@@ -0,0 +1,13 @@
+package tech.budgetforthemasses.budgetforthemasses;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainOutput extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.output_main);
+
+    }
+}
