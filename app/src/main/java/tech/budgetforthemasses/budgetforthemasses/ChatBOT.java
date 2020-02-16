d85e1ec7827fabb9bf6efac0763c9dea7d69820c
Jawad Arshad <56083056+HamJaw1432@users.noreply.github.com>
2020-02-16 10:48:59 -0500

    chatbox input

--- app/src/main/java/tech/budgetforthemasses/budgetforthemasses/ChatBOT.java
+++ app/src/main/java/tech/budgetforthemasses/budgetforthemasses/ChatBOT.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ChatBOT extends AppCompatActivity {
 
@@ -16,13 +18,16 @@ public class ChatBOT extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button enter_button = (Button)findViewById(R.id.enter_button);
+        enter_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                EditText user_input = (EditText) findViewById(R.id.user_input_text);
+                
+
             }
+
         });
     }
 }
