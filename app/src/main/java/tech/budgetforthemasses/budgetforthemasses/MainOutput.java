fd742ffc2379214ed17b19a2ff3e2586e971dfe0
Jawad Arshad <56083056+HamJaw1432@users.noreply.github.com>
2020-02-16 10:33:40 -0500

    Update MainOutput.java

--- app/src/main/java/tech/budgetforthemasses/budgetforthemasses/MainOutput.java
+++ app/src/main/java/tech/budgetforthemasses/budgetforthemasses/MainOutput.java
@@ -21,15 +21,6 @@ public class MainOutput extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         Intent i = getIntent();
         String food = i.getStringExtra("food");
         String rent = i.getStringExtra("rent");
@@ -67,6 +58,16 @@ public class MainOutput extends AppCompatActivity {
                 startActivity(startIntent);
             }
         });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), ChatBOT.class);
+                startActivity(startIntent);
+            }
+        });
+
 
     }
 
